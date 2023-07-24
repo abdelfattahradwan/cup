09d03e90415f75366d3769d21e6407fdf1fed290
Abdelfattah-Radwan <ARadwan620@gmail.com>
2023-07-24 16:22:48 +0300

    Added PathTypeDeductionException.cs

---
+++ PathTypeDeductionException.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+namespace CreateUnityPackage;
+
+[Serializable]
+public sealed class PathTypeDeductionException : Exception
+{
+	public PathTypeDeductionException(string path) : base($"Unable to deduce the path type. Path: '{path}'")
+	{
+		Path = path;
+	}
+	
+	private PathTypeDeductionException(SerializationInfo info, StreamingContext context) : base(info, context)
+	{
+		Path = info.GetString(nameof(Path));
+	}
+
+	public string? Path { get; }
+	
+	public override void GetObjectData(SerializationInfo info, StreamingContext context)
+	{
+		base.GetObjectData(info, context);
+		
+		info.AddValue(nameof(Path), Path, typeof(string));
+	}
+}
